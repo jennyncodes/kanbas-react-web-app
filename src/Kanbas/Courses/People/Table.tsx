7f206e149db7e30960fc9922662f2585d69cd51e
Jenny Nguyen <jennayx14@yahoo.com>
2024-12-08 10:07:42 -0800

    finalized a6

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -5,37 +5,64 @@ import PeopleDetails from "./Details";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import * as coursesClient from "../client";
+import axios from "axios";
 
 export default function PeopleTable({ users = [] }: { users?: any[] }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const { cid } = useParams<{ cid: string }>();  // Get course ID from URL
+  const { cid } = useParams();
   const [courseUsers, setCourseUsers] = useState<any[]>([]);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const fetchUsersForCourse = async () => {
+    const fetchUsers = async () => {
       try {
-        const usersForCourse = await coursesClient.findUsersForCourse(cid as string);
-        setCourseUsers(usersForCourse);  
+        if(currentUser?.role === "ADMIN") {
+          setIsAdmin(true);
+          const response = await axios.get("/api/users", { withCredentials: true });
+          setCourseUsers(response.data);
+        } else if (cid as string) {
+          const usersForCourse = await coursesClient.findUsersForCourse(cid as string);
+          setCourseUsers(usersForCourse);  
+        }
       } catch (error) {
         console.error("Error fetching users for course:", error);
       }
     };
 
     if (cid) {
-      fetchUsersForCourse();
+      fetchUsers();
     }
-  }, [cid]);
+  }, []);
 
   return (
     <div id="wd-people-table">
          <PeopleDetails />
+         <br/><br/>
+         <h3>{currentUser?.role === "ADMIN" ? "All Users (Admin View)" : "Course Users"}</h3>
       <table className="table table-striped">
         <thead>
           <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
         </thead>
         <tbody>
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+              <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
 
-              {users.map((user: any) => (
+                <FaUserCircle className="me-2 fs-1 text-secondary" />
+                <span className="wd-first-name">{user.firstName}</span>
+                <span className="wd-last-name">{user.lastName}</span>
+                </Link>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
+
+          {courseUsers.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
               <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
@@ -54,34 +81,5 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
           ))}
           </tbody>
           </table>
-
-  {courseUsers.map((user: any) => (
-    <table className="table table-striped">
-        <thead>
-          <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
-        </thead>
-        <tbody>
-            
-            <tr key={user._id}>
-              <td className="wd-full-name text-nowrap">
-              <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
-
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName}</span>
-                <span className="wd-last-name">{user.lastName}</span>
-                </Link>
-              </td>
-              <td className="wd-login-id">{user.loginId}</td>
-              <td className="wd-section">{user.section}</td>
-              <td className="wd-role">{user.role}</td>
-              <td className="wd-last-activity">{user.lastActivity}</td>
-              <td className="wd-total-activity">{user.totalActivity}</td>
-            </tr>
-          
-      </tbody>
-      </table>
-      ))}
-      
-
-
+  
     </div> );}
--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -26,6 +26,7 @@ export default function Dashboard({
 }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const dispatch = useDispatch();
+  console.log(courses);
 
   return (
     <div id="wd-dashboard">
